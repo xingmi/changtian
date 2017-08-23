2e0cde2b00e7721f0397d23d49d0bd97fe30b800
luliangshu <lu3501037@gmail.com>
2017-08-23 23:42:44 +0800

    modify

---
+++ res/controllers/my.js
@@ -0,0 +1,32 @@
+var Vue = require('vue');
+var utility = require('../config/utility');
+var Config = require('../config/globalMain');
+var Toast = require('../widget/toast');
+
+require('../config/date');
+
+new Vue({
+    el : '.my',
+    data : {
+      user: {}
+    },
+    created : function(){
+      this.getData()
+    },
+    methods : {
+      getData : function(){        
+        this.$http.get(Config.api + 'distributor/info?openid=' + localStorage['openId'])
+        .then(function(res){
+            if(res.body.code == 0){   
+              this.user = res.body.data
+            }else{
+              Toast.show('请先注册成为代理商',2000,function(){
+
+              })
+            }
+        },function(){
+
+        }).bind(this);
+      }
+    }
+});
